e632315698a180ecd6887f4283a12db9a2159f6a
Yonas Berhe <yonas.berhe007@gmail.com>
2016-04-16 16:19:51 -0700

    json to one object next identify image, url, Title

--- view/app.js
+++ view/app.js
@@ -1,34 +1,60 @@
-var subreddits = {  artificial: 'artificial', seattle: 'Seattle'};
+var subreddits = [
+    'artificial',
+    'Seattle'
+];
+//console.log(subreddits);
 
-console.log(subreddits);
 
 var dataMerged = {};
+
+
 
 
 //dataMerged[subreddit] = [];
 //dataMerged['dataisbeautiful'] = [];
 
-console.log(dataMerged);
+//console.log(dataMerged);
 
 
 
 
-$.ajax ({
-
-url: 'https://www.reddit.com/r/' + subreddits + '.json?limit=100',
-dataType: 'jsonp',
-jsonp: 'jsonp',
-success: function (data) {
-
-
-
-//var json = data.data.children;
-//console.log(json);
-
-
-}
-
+/*subreddits.forEach(function(subreddit){
+   $.ajax({
+        url: 'https://www.reddit.com/r/' + subreddit + '.json?limit=100',
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        success: function(data) {
+            var json = data.data.children;
+            //console.log(json);
+            dataMerged += json;
+            console.log(dataMerged);
+        }
+    })
 })
 
+*/
 
+for (var i = 0; i < 2; i++) {
 
+    var subredditPoint = i;
+
+    var subreddit;
+    if (subreddit == 1) {
+
+        subreddit = subreddits.one;
+
+    } else {
+        subreddit = subreddits.two;
+    }
+    $.ajax({
+        url: 'https://www.reddit.com/r/' + subreddit + '.json?limit=100',
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        success: function(data) {
+            var json = data.data.children;
+            //console.log(json);
+            dataMerged += json;
+            console.log(dataMerged);
+        }
+    })
+}
