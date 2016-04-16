43f220c8d190fd61ebc5cb642434646634dc9444
Yonas Berhe <yonas.berhe007@gmail.com>
2016-04-16 15:05:11 -0700

    code

--- view/app.js
+++ view/app.js
@@ -1,1 +1,15 @@
-console.log("client side code");
+var subreddit = 'earchporn';
+
+
+$.ajax ({
+
+url: 'https://www.reddit.com/r/' + subreddit + '.json?limit=100',
+dataType: 'json',
+success: function (data) {
+
+ var jsonv = data.children;
+
+
+}
+
+})
