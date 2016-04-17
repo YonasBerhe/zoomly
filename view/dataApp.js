86b61f7714537b49c4787a35d8571b87126987dd
Yonas Berhe <yonas.berhe007@gmail.com>
2016-04-16 18:34:19 -0700

    added

---
+++ view/dataApp.js
@@ -0,0 +1,24 @@
+
+var dataMerged = {}; 
+
+subreddits.forEach(function(subreddit){
+   $.ajax({
+       type: "POST",
+        url: 'https://www.reddit.com/r/' + subreddit + '.json?limit=100',
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        success: function(data) {
+            var json = data.data.children;
+             dataMerged[subreddit] = json;
+//console.log(dataMerged);
+//     console.log(dataMerged.Seattle[0].data.url);
+           for (i = 0; i < 100; i++) {
+console.log(dataMerged.dataisbeautiful[i].data.title);
+console.log(dataMerged.dataisbeautiful[i].data.url);
+
+            }
+
+        }
+    })
+})
+
