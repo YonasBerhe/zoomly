94383595e86df3deb0fc28cdb17fddfa4d36f668
Yonas Berhe <yonas.berhe007@gmail.com>
2016-04-17 12:02:13 -0700

    data is up

---
+++ src/parseData.js
@@ -0,0 +1,11 @@
+var content = require("./hnDownload");
+//console.log(content);
+console.log(content.shortname);
+
+
+content.shortname.forEach(function(news) {
+console.log("hello world");
+
+});
+
+
