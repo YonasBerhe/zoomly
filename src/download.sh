653ff4824e06990a5431cff5ee4f9bb97f3b2e0a
Jason Francis <jason@halfspiral.com>
2016-04-16 22:10:05 -0700

    add image width and height to subreddit data

--- src/download.sh
+++ src/download.sh
@@ -1,1 +1,3 @@
-curl https://www.reddit.com/r/$1.json?limit=100 -s | jq '[.data.children[].data | {title: .title, url: .permalink, image: .preview.images[0].source.url}]' -M
+#!/bin/bash
+
+curl https://www.reddit.com/r/$1.json?limit=100 -s | jq '[.data.children[].data | {title: .title, url: .permalink, image: .preview.images[0].source.url, width: .preview.images[0].source.width, height: .preview.images[0].source.height}]' -M
