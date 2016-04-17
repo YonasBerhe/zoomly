4b44d84f1e77a9ea9b30c53f95f4f3e47fd6bc43
PolarisJunior <brownasianpj@yahoo.com>
2016-04-16 19:51:52 -0500

    merge fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //2432e2a1-173d-42df-9f7a-0f118ea73ae7
 
 const express = require("express");
-const serveStatic = require("serve-static");
 var app = express();
+var PORT = 3000;
 
 app.use(express.static('view'));
 
-app.listen(3000);
+app.listen(PORT);
