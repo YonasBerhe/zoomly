fc34a83289c4e3a1c97ce9d3776d4bddd32dcff2
Jason Francis <jason@halfspiral.com>
2016-04-16 18:00:58 -0700

    move frontend files into view folder

--- view/app.js
+++ view/app.js
@@ -1,22 +1,37 @@
-var subreddits = ['artificial', 'Seattle' ];
-//console.log(subreddits);
-//var subreddits = [
-//'artificial', 
-//'seattle'
-//]
+var diameter = 4;
 
-var dataMerged = {};
+var color = d3.scale.linear()
+    .domain([-1, 5])
+    .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+    .interpolate(d3.interpolateHcl);
 
-subreddits.forEach(function(subreddit){
-   $.ajax({
-       type: "POST",
-        url: 'https://www.reddit.com/r/' + subreddit + '.json?limit=100',
-        dataType: 'jsonp',
-        jsonp: 'jsonp',
-        success: function(data) {
-            var json = data.data.children;
-            dataMerged[subreddit] = json;
-            console.log(dataMerged);
-        }
-    })
-})
+var pack = d3.layout.pack()
+    .padding(2)
+    .size([diameter, diameter])
+    .value(function(d) { return d.size; })
+
+var scene = d3.select("a-entity#objects");
+
+d3.json("flare.json", function(error, root) {
+  if (error) throw error;
+
+  var focus = root,
+      nodes = pack.nodes(root),
+      view;
+
+  var circle = scene.selectAll("a-cylinder")
+      .data(nodes)
+    .enter().append("a-cylinder")
+      .attr("position", function(d) {
+        return d.x + " " + (d.depth) * 0.2 + " " + d.y;
+      })
+      .attr("radius", function(d) { return d.r; })
+      .attr("height", 0.2)
+      .attr("color", function(d) {
+        return d.children ? color(d.depth) : "gray";
+      })
+      .attr("roughness", 0.8)
+    .append("a-event")
+      .attr("name", "cursor-mouseenter")
+      .attr("color", "orange");
+});
