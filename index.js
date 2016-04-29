c0ad4987dd03562ac93ba86b463908fcac2c83d3
Yonas Berhe <yonas.berhe007@gmail.com>
2016-04-29 12:52:00 -0700

    pointing back to cylinder view

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 7000;
 const bodyParser = require('body-parser');
 const main = require('./src/main.js');
 
-app.use(express.static('view'));
+//app.use(express.static('view'));
 app.use(express.static('cylinders'));
 app.use('/node_modules', express.static('node_modules'));
 
